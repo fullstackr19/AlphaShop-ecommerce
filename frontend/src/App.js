f8b2596af7b1b6102a1e6ceaf493230c5d0a99e7
Rehan Ankalgi <Rehanankalgi7t2@gmail.com>
2021-06-13 17:21:09 +0530

    added navbar, react-bootstrap setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-function App() {
+import { Container } from 'react-bootstrap';
+
+const App = () => {
   return (
     <>
-      <h1>Alpha Shoppe</h1>
+      <Header/>
+      <main className='py-5'>
+        <Container>
+          <h1>Alpha Shoppe</h1>
+        </Container>
+      </main>
+      <Footer/>
     </>
   );
 }
