6f99b40f89c6d2ae0468742a68b9229fc4dd2518
Rehan Ankalgi <Rehanankalgi7t2@gmail.com>
2021-06-14 15:16:59 +0530

    implemented frontend routes, Individual product screen, rating component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 
 import { Nav, Navbar, Container } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap' 
 
 const Header = () => {
     return (
         <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">Alphashop.</Navbar.Brand>
+                <LinkContainer to="/">
+                    <Navbar.Brand>Alphashop.</Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                        <Nav.Link href="/cart"><i class='fas fa-shopping-cart'></i> Cart</Nav.Link>
-                        <Nav.Link href="/login"><i class='fas fa-user'></i> Sign in</Nav.Link>
+                        <LinkContainer to="cart">
+                            <Nav.Link><i class='fas fa-shopping-cart'></i> Cart</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to="/login">
+                            <Nav.Link><i class='fas fa-user'></i> Sign in</Nav.Link>
+                        </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
