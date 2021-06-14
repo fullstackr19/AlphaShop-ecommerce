a28829daef128f6244b0e3086dc131148a0e775f
Rehan Ankalgi <Rehanankalgi7t2@gmail.com>
2021-06-14 07:57:23 +0530

    Product components, Homescreen & product data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <Header/>
       <main className='py-5'>
         <Container>
-          <h1>Alpha Shoppe</h1>
+          <HomeScreen/>
         </Container>
       </main>
       <Footer/>
